b9b4c8ddd191185a8628304a2de2dbb041efb1b2
Arturo Benitez <arturobenitez@Mac.lan>
2024-10-19 11:38:29 -0400

    cleaning up file tree

--- scripts.js
+++ scripts.js
@@ -1,20 +1,20 @@
 const images = [
-    "art1.jpg",
-    "art2.jpg",
-    "art3.jpg",
-    "art4.jpg",
-    "bronx.jpg",
-    "deskspace.jpg",
-    "fuji.jpg",
-    "hand.jpg",
-    "hitchcock.jpg",
-    "jwebb.jpg",
-    "minecraft.jpg",
-    "monitor.jpg",
-    "next.jpg",
-    "shuttle.jpg",
-    "sr71.jpg",
-    "terminator.jpg"
+    "images/art1.jpg",
+    "images/art2.jpg",
+    "images/art3.jpg",
+    "images/art4.jpg",
+    "images/bronx.jpg",
+    "images/deskspace.jpg",
+    "images/fuji.jpg",
+    "images/hand.jpg",
+    "images/hitchcock.jpg",
+    "images/jwebb.jpg",
+    "images/minecraft.jpg",
+    "images/monitor.jpg",
+    "images/next.jpg",
+    "images/shuttle.jpg",
+    "images/sr71.jpg",
+    "images/terminator.jpg"
 ];
 
 const randomImage = images[Math.floor(Math.random() * images.length)];
