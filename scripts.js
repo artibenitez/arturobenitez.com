6c4d88b514950cd5d61893c40ce6fe94c87924d0
Arturo Benitez <arturobenitez@Artis-MacBook-Pro.local>
2024-10-28 14:52:13 -0400

    header image changes

--- scripts.js
+++ scripts.js
@@ -2,13 +2,6 @@ const images = [
     "images/header1.jpg",
     "images/header2.jpg",
     "images/header3.jpg",
-    "images/header4.jpg",
-    "images/header5.jpg",
-    "images/header6.jpg",
-    "images/header7.jpg",
-    "images/header8.jpg",
-    "images/header9.jpg",
-    "images/header10.jpg",
 ];
 
 const randomImage = images[Math.floor(Math.random() * images.length)];
